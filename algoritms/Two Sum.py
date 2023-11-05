fdb282859374b3fe208442cc5ca6db33a4b53593
Oleg <119931573+oleg14038@users.noreply.github.com>
2023-11-05 20:35:32 +0900

    Contains Duplicate

--- algoritms/Two Sum.py
+++ algoritms/Two Sum.py
@@ -9,7 +9,7 @@ from typing import List
 #        for i in range(len(nums)): #Первый for проходиться по всем элемнентам, len - проверяем длинну массива (nums), range - переберает от 0 до 3 элемента нашего массива, i - указывает на первый элементам
 #            for j in range(len(nums)): #После 1 прохождения for, for проходить еще раз,  j - указывает на первый элемент массива 
 #                if nums[i] + nums[j] == target and i != j: #складываем элементы массива, если они в сумму доют 9 и элементы массива не равны 
- #                   return[i,j] #возращаем элементы 
+#                   return[i,j] #возращаем элементы 
 #        return[] # None 
 #c = ClassSolutin() # экземпляр класса Child 
 #c.twoSum() #метод переопределен классом наследнико
@@ -21,17 +21,17 @@ from typing import List
 
 class ClassSolutin:# объявляем  класс
     def twoSum(self, nums: List[int], target: int,) -> List[int]: 
-         hashmap = {}# 2 : 0 
+        hashmap = {}# 2 : 0 
                      # 6 : 1
                      # 5 : 2 
                      # 8 : 3 
-                     #11 : 4   
-         for i in range(len(nums)):
+                     #11 :for i in range(len(nums)):
+        for i in range(len(nums)):
              hashmap[[i]] = i 
         print(hashmap)
         for i in range(len(nums)):
             complent = target - nums[i]#Пример: получаем 12 
-            if complent in hashmap and i != hashmap(complent):#Проверяем есть ли 12 в нашешем hashamp b и пытаемся вернуть одно и тоже число  
+            if complent in hashmap and i != hashmap(complent):#Проверяем есть ли 12 в нашешем hashamp b и не пытаемся вернуть одно и тоже число - i != hashmap(complent)  
                 return[i, hashmap[complent]]
             hashmap[nums[i]] = i 
 
