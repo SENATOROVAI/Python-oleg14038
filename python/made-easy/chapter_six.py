3671b8cfe51eb424f5539368a329062cf41d9f54
Oleg <119931573+oleg14038@users.noreply.github.com>
2024-11-25 20:36:07 +0900

    fixing_bug_pre

--- python/made-easy/chapter_six.py
+++ python/made-easy/chapter_six.py
@@ -1,20 +1,5 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.4
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-"""Chapter4."""
-
-# +
-import math
+"""
+Chapter 6: Python Basics.
 
 import numpy as np
 
